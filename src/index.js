e40ce6cbc4e500bc93c80816507294464d0bdeeb
munroe1786 <era3munroe@gmail.com>
2021-07-26 21:16:19 -0500

    delete project structure notes from index.js / Change firebase uid and other info to my own

--- src/index.js
+++ src/index.js
@@ -3,20 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// client side rendered app: react (create-react-app)
-// database which is Firebase
-// react-loading-skeleton
-// tailwind
-
-// folder structure
-// -> src
-// -> -> components
-// -> -> constants
-// -> -> context
-// -> -> helpers
-// -> -> hooks
-// -> -> pages
-// -> -> lib (firebase is going to live in here)
-// -> -> services (firebase functions in here)
-// -> -> styles (tailwind's folder (app/tailwind))
