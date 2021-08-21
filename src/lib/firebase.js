b2e34045fe959c0210611dc1a966fb1232b48fe1
munroe1786 <era3munroe@gmail.com>
2021-08-20 22:35:28 -0500

    delete seed import functionality from firebase.js/lib so it's not added to db more than once

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,10 +2,6 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// here is where I want to import the seed file
-
-import { seedDatabase } from '../seed';
-
 const config = {
   apiKey: 'AIzaSyBCNOLK5jyBMKNXOi-yFvHUfJNDJy3YsRM',
   authDomain: 'instagram-31a21.firebaseapp.com',
@@ -17,10 +13,5 @@ const config = {
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
-
-// here is where I want to call the seed file (only ONCE)
-// seedDatabase(firebase);
-
-console.log('firebase', firebase);
 
 export { firebase, FieldValue };
