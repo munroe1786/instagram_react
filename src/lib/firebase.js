ae64d8450451064536abc0345efd2766f1e8daf8
munroe1786 <era3munroe@gmail.com>
2021-08-20 22:01:13 -0500

    change double quotes to single and fix margins for prettier functioning

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 // here is where I want to import the seed file
 
 const config = {
-    apiKey: "AIzaSyBCNOLK5jyBMKNXOi-yFvHUfJNDJy3YsRM",
-    authDomain: "instagram-31a21.firebaseapp.com",
-    projectId: "instagram-31a21",
-    storageBucket: "instagram-31a21.appspot.com",
-    messagingSenderId: "892942058224",
-    appId: "1:892942058224:web:268e22abd657fdb3a5b835"
+  apiKey: 'AIzaSyBCNOLK5jyBMKNXOi-yFvHUfJNDJy3YsRM',
+  authDomain: 'instagram-31a21.firebaseapp.com',
+  projectId: 'instagram-31a21',
+  storageBucket: 'instagram-31a21.appspot.com',
+  messagingSenderId: '892942058224',
+  appId: '1:892942058224:web:268e22abd657fdb3a5b835'
 };
 
 const firebase = Firebase.initializeApp(config);
@@ -18,5 +18,7 @@ const { FieldValue } = Firebase.firestore;
 
 // here is where I want to call the seed file (only ONCE)
 // seedDatabase(firebase);
+
+console.log('firebase', firebase);
 
 export { firebase, FieldValue };
