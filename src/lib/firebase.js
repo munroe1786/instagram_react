e2c12f7824c252226c64eaf4d8d48939c0a1fec4
munroe1786 <era3munroe@gmail.com>
2021-08-20 21:51:31 -0500

    add firebase config info to lib/firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,7 +4,14 @@ import 'firebase/auth';
 
 // here is where I want to import the seed file
 
-const config = {};
+const config = {
+    apiKey: "AIzaSyBCNOLK5jyBMKNXOi-yFvHUfJNDJy3YsRM",
+    authDomain: "instagram-31a21.firebaseapp.com",
+    projectId: "instagram-31a21",
+    storageBucket: "instagram-31a21.appspot.com",
+    messagingSenderId: "892942058224",
+    appId: "1:892942058224:web:268e22abd657fdb3a5b835"
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
