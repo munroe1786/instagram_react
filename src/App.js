941fd744683996989dc91d39da18180071df84b8
munroe1786 <era3munroe@gmail.com>
2021-08-21 22:39:46 -0500

    add switch statement to App.js and import lazy into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy;
 
 function App() {
   return (
     <Router>
-      
+      <Switch>
+        <Route path="/login" component={} />
+      </Switch>
     </Router>
   )
 }
